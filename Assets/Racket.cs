6f11f8f838e941569b11da9fbc6d10103ce452e2
CristianJRamirez <45858000w@iespoblenou.org>
2017-03-24 16:00:06 +0100

    Arkanoid unity acabado

---
+++ Assets/Racket.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Racket : MonoBehaviour {
+
+	public float speed = 500.0f;
+
+		//utilitzarem FixedUpdate quan treballem en fisiques
+	void FixedUpdate()
+	{
+		float horizontalDirection = Input.GetAxisRaw("Horizontal");
+
+		GetComponent <Rigidbody2D>().velocity = Vector2.right * horizontalDirection * speed;
+	}
+	
+}
